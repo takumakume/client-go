be91e15b027e1cd9f0f6a504eab53b943a474df2
久米 拓馬 <takuma.kume@pepabo.com>
2023-08-29 11:41:31 +0900

    Add AddTag/DeleteTag to PolicyService

--- policy.go
+++ policy.go
@@ -94,3 +94,23 @@ func (ps PolicyService) DeleteProject(ctx context.Context, policyUUID, projectUU
 	_, err = ps.client.doRequest(req, &p)
 	return
 }
+
+func (ps PolicyService) AddTag(ctx context.Context, policyUUID uuid.UUID, tagName string) (p Policy, err error) {
+	req, err := ps.client.newRequest(ctx, http.MethodPost, fmt.Sprintf("/api/v1/policy/%s/project/%s", policyUUID, tagName))
+	if err != nil {
+		return
+	}
+
+	_, err = ps.client.doRequest(req, &p)
+	return
+}
+
+func (ps PolicyService) DeleteTag(ctx context.Context, policyUUID uuid.UUID, tagName string) (p Policy, err error) {
+	req, err := ps.client.newRequest(ctx, http.MethodDelete, fmt.Sprintf("/api/v1/policy/%s/project/%s", policyUUID, tagName))
+	if err != nil {
+		return
+	}
+
+	_, err = ps.client.doRequest(req, &p)
+	return
+}
