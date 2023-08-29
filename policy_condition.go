3e85753cfb6d19b00c09e9af1f4543ca8576e281
久米 拓馬 <takuma.kume@pepabo.com>
2023-08-29 11:29:07 +0900

    Add PolicyConditionService
    Signed-off-by: 久米 拓馬 <takuma.kume@pepabo.com>

--- policy_condition.go
+++ policy_condition.go
@@ -1,11 +1,51 @@
 package dtrack
 
-import "github.com/google/uuid"
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/google/uuid"
+)
 
 type PolicyCondition struct {
-	UUID     uuid.UUID
 	Policy   *Policy `json:"policy,omitempty"`
 	Operator string  `json:"operator"`
 	Subject  string  `json:"subject"`
 	Value    string  `json:"value"`
+	UUID     uuid.UUID               `json:"uuid,omitempty"`
+}
+
+type PolicyConditionService struct {
+	client *Client
+}
+
+func (pcs PolicyConditionService) Create(ctx context.Context, policyUUID uuid.UUID, policyCondition PolicyCondition) (p PolicyCondition, err error) {
+	req, err := pcs.client.newRequest(ctx, http.MethodPut, fmt.Sprintf("/api/v1/policy/%s/condition", policyUUID), withBody(policyCondition))
+	if err != nil {
+		return
+	}
+
+	_, err = pcs.client.doRequest(req, &p)
+	return
+}
+
+func (pcs PolicyConditionService) Update(ctx context.Context, policyCondition PolicyCondition) (p PolicyCondition, err error) {
+	req, err := pcs.client.newRequest(ctx, http.MethodPost, "/api/v1/policy/condition", withBody(policyCondition))
+	if err != nil {
+		return
+	}
+
+	_, err = pcs.client.doRequest(req, &p)
+	return
+}
+
+func (pcs PolicyConditionService) Delete(ctx context.Context, policyConditionUUID uuid.UUID) (err error) {
+	req, err := pcs.client.newRequest(ctx, http.MethodDelete, fmt.Sprintf("/api/v1/policy/condition/%s", policyConditionUUID))
+	if err != nil {
+		return
+	}
+
+	_, err = pcs.client.doRequest(req, nil)
+	return
 }
